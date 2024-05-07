3d33522d4673ac31b9b9ec079877c51a15bdb4c2
Anna <119071299+heyanja@users.noreply.github.com>
2024-05-07 10:31:57 -0400

    Initial Commit

---
+++ NextTrackAppleMusic/NextTrackAppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextTrackAppleMusicApp.swift
+//  NextTrackAppleMusic
+//
+//  Created by Anna Fadieieva on 2024-05-07.
+//
+
+import SwiftUI
+
+@main
+struct NextTrackAppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
